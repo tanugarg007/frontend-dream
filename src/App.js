43f624a2ddd6abd675840bf518839a922492bd54
tanugarg007 <tanugarg252@gmail.com>
2026-02-16 23:56:36 +0530

    remove unused vars and hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 import Approutes from "./Routes/Approutes";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
