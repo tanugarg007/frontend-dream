dc4889d334fcdee2c3c472837d9fd0296e7d4967
tanugarg007 <tanugarg252@gmail.com>
2026-02-17 21:44:33 +0530

    fix vercel build

--- src/Component/GraphicandUIUX.jsx
+++ src/Component/GraphicandUIUX.jsx
@@ -318,6 +318,9 @@ const GraphicandUIUX = () => {
 
         {/* Footer */}
         <div className="w-full min-h-[360px] px-4 md:px-0 mt-10 ">
+           <div className="w-full h-[30px]">
+               <hr className="border-slate-700 w-full md:w-[88%] mt-7 md:ml-24" />
+             </div>
           <div className="w-full flex flex-col md:flex-row md:justify-between md:items-start md:h-[250px] md:px-20">
             {/* Logo + Social */}
             <div className="w-full md:w-[280px] mb-4 md:mb-0 mt-4">
