32f6139881f09acd09f86423213123cd80ed8329
tanugarg007 <tanugarg252@gmail.com>
2026-03-13 16:57:08 +0530

    changeimage

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -22,10 +22,7 @@ const Index = () => {
 
   return (
     <div className="w-full ">
-    <div
-  className="relative w-full min-h-screen bg-fixed bg-cover bg-center bg-no-repeat lg:min-h-screen"
-  style={{ backgroundImage: `url(${logo9})`, backgroundAttachment: 'fixed' }}
->
+      <div className="relative w-full min-h-screen bg-fixed bg-cover bg-center bg-no-repeat lg:min-h-screen " style={{ backgroundImage: `url(${logo9})`, backgroundAttachment: 'fixed' }}>
         <Navbar onEnquiryClick={() => window.openEnquiry?.()} />
         <div className="w-full  mt-[70px]">
           <img src={logo10} alt="animex" className="w-full" />
